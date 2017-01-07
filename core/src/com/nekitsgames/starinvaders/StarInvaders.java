2729bd00561dacee74abf99f00557e48ad4d4bb5
Nikita Serba <nikitaserba@gmail.com>
2017-01-07 20:49:40 +0200

    Add space sound playing

--- core/src/com/nekitsgames/starinvaders/StarInvaders.java
+++ core/src/com/nekitsgames/starinvaders/StarInvaders.java
@@ -2,18 +2,33 @@ package com.nekitsgames.starinvaders;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class StarInvaders extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture shipImage;
+	Texture asteroidSmallIMage;
+	Texture asteroidBigImage;
+	Sound lazerSound;
+	Music spaceSound;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("ship.png");
+
+		shipImage = new Texture("ship.png");
+		asteroidSmallIMage = new Texture("asteroid_small.png");
+		asteroidBigImage = new Texture("asteroid_big.png");
+
+		lazerSound = Gdx.audio.newSound(Gdx.files.internal("lazer_sound.wav"));
+		spaceSound = Gdx.audio.newMusic(Gdx.files.internal("space_sound.mp3"));
+
+		spaceSound.setLooping(true);
+		spaceSound.play();
 	}
 
 	@Override
@@ -21,13 +36,16 @@ public class StarInvaders extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		shipImage.dispose();
+		asteroidBigImage.dispose();
+		asteroidSmallIMage.dispose();
+		spaceSound.dispose();
+		lazerSound.dispose();
 	}
 }
