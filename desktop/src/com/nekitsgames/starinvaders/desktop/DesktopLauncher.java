84047a28c7bdb3a401297c7d4b08818a2502402a
Nikita Serba <nikitaserba@gmail.com>
2017-01-07 20:24:59 +0200

    Add default PC config

--- desktop/src/com/nekitsgames/starinvaders/desktop/DesktopLauncher.java
+++ desktop/src/com/nekitsgames/starinvaders/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.nekitsgames.starinvaders.StarInvaders;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Star Invaders";
+		config.width = 1920;
+		config.height = 1080;
+		config.fullscreen = true;
+		config.allowSoftwareMode = true;
 		new LwjglApplication(new StarInvaders(), config);
 	}
 }
